7fd274cef3c34c0b5aeae3d991cbdd0d66ed7884
UoSCompSoc <compsoc@firstyearmatters.info>
2018-03-02 16:19:41 +0000

    tidied up

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 public class Main
 {
+	/**
+	 * Provide the program with a static entrance point which creates a new Main object
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
 		new Main();
 	}
 
+	/**
+	 * Create a new Main object
+	 * Constructor private as nothing else should create a Main object
+	 */
 	private Main()
 	{
 		Reader r = new Reader("data/example.in");
 		Pizza pizza = r.readFile();
 
-		pizza.printPizzaDetails();
+		pizza.printDetails();
 	}
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -6,6 +6,12 @@ public class Pizza
 	private int max;
 	private char[][] pizzaGrid;
 
+	/**
+	 * Create a new Pizza object
+	 * @param min
+	 * @param max
+	 * @param pizza
+	 */
 	public Pizza(int min, int max, char[][] pizza)
 	{
 		this.rows = pizza.length;
@@ -16,34 +22,57 @@ public class Pizza
 		pizzaGrid = pizza;
 	}
 
+	/**
+	 * get the number of rows the pizza has
+	 * @return int
+	 */
 	public int getRows()
 	{
 		return rows;
 	}
 
+	/**
+	 * get the number of columns the pizza has
+	 * @return int
+	 */
 	public int getColumns()
 	{
 		return columns;
 	}
 
+	/**
+	 * get the minimum number of topping count per slice
+	 * @return int
+	 */
 	public int getMin()
 	{
 		return min;
 	}
 
+	/**
+	 * get the maximum number of topping count per slice
+	 * @return
+	 */
 	public int getMax()
 	{
 		return max;
 	}
 
+	/**
+	 * get the char grid representing the pizza
+	 * @return char[][]
+	 */
 	public char[][] getPizzaGrid()
 	{
 		return pizzaGrid;
 	}
 
-	public void printPizzaDetails()
+	/**
+	 * Print information about the pizza in a formatted manner
+	 */
+	public void printDetails()
 	{
-		System.out.println(this + ":\t" + rows + "x" + columns + "\tMin: " + min + "\tMax: " + max);
+		System.out.println(rows + "*" + columns + "\t\tMin: " + min + "\t\tMax: " + max);
 		for (int i = 0; i < pizzaGrid.length; i++)
 		{
 			for (int j = 0; j < pizzaGrid[i].length; j++)
